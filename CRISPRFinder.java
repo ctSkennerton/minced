bb70cadf2a2b7a4da5a1eba9fe2c60d1b61c0627
ctSkennerton <c.skennerton@gmail.com>
2013-11-18 23:10:35 +1000

    Minor output format changes

--- CRISPRFinder.java
+++ CRISPRFinder.java
@@ -277,7 +277,7 @@ public class CRISPRFinder
                 currCRISPR = (CRISPR)CRISPRVector.elementAt(k);
                 if(outputformat > 0) {
                 	String crispr_id = "CRISPR" + (++totalCrisprCount);
-                	out.print(sequence.getName() + "\tminced\tCRISPR\t");
+                	out.print(sequence.getName() + "\tminced:" + minced.VERSION + "\tCRISPR\t");
                     out.print((currCRISPR.start() + 1) + "\t" + (currCRISPR.end() + 2) + "\t");
                     out.print(currCRISPR.numRepeats() + "\t.\t.\tID="+ crispr_id);
                     out.print("\n");
