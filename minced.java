7327a7b31b38856e5e441a0defee08ea7d5eee50
ctSkennerton <c.skennerton@gmail.com>
2018-05-08 13:06:05 -0700

    enough changes to call this 0.3.0

--- minced.java
+++ minced.java
@@ -2,7 +2,7 @@ import java.io.*;
 
 public class minced
 {
-   public static final String VERSION = "0.2.2";
+   public static final String VERSION = "0.3.0";
 	public static void main(String[] args)
    {
       //default values
