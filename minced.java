35d5f72be1f39ce26fa724d1a15cc4eff4992677
ctSkennerton <c.skennerton@gmail.com>
2018-05-08 12:44:13 -0700

    update version number

--- minced.java
+++ minced.java
@@ -2,7 +2,7 @@ import java.io.*;
 
 public class minced
 {
-   public static final String VERSION = "0.2.1";
+   public static final String VERSION = "0.2.2";
 	public static void main(String[] args)
    {
       //default values
