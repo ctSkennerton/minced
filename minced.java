1db340f3d6bfaccb2f30d4cbfbef5e6a03883d60
Connor Skennerton <c.skennerton@gmail.com>
2019-09-02 18:49:58 -0700

    update the version string

--- minced.java
+++ minced.java
@@ -2,7 +2,7 @@ import java.io.*;
 
 public class minced
 {
-   public static final String VERSION = "0.4.0";
+   public static final String VERSION = "0.4.2";
 	public static void main(String[] args)
    {
       //default values
